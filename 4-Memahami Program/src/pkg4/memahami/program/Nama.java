935db94c79818efc70ecb8f886d1367c6d653f39
rizalkharyadi <Rizal083822286857@gmail.Com>
2019-12-20 18:57:55 +0700

    Nanda

---
+++ 4-Memahami Program/src/pkg4/memahami/program/Nama.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg4.memahami.program;
+
+/**
+ * Nama : ANANDA FRIZKY MAULANA
+ * NIM  : A2.1900016S
+ * Kelas: TI-1C
+ */
+public class Nama {
+    public static void main(String[] args){
+		System.out.print("nama ku adalah Dani, teman si fajar");
+	}
+}
---
+++ 5-Organisasi Program dengan Package/src/Program_manual/Main.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Program_manual;
+
+/**
+ * Nama : ANANDA FRIZKY MAULANA 
+ * NIM  : A2.1900016
+ * Kelas: TI-1C
+ */
+public class Main {
+    public static void main(String[] args){
+		System.out.print("nama ku adalah Dani, teman si fajar");
+	}
+}
